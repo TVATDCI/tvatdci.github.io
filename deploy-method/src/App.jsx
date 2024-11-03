4b31b516f30cce6d9b2cafc8d692bd8938a38879
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2024-11-03 18:06:26 +0100

    getAdvice fnct added

--- deploy-method/src/App.jsx
+++ deploy-method/src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [advice, setAdvice] = useState("");
+  const [error, setError] = useState(null);
+
+  // getAdvice function using method async await to fetch data
+  async function getAdvice() {
+    try {
+      const response = await fetch(`https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`);
+      {/* It seems that the API is not working properly, so I have to add a timestamp to the API and URL as parameter to avoid the cache busting */ }
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data.slip.advice);
+      setAdvice(data.slip.advice);
+      setError(null);
+    } catch (error) {
+      setError("Failed to fetch advice. Please try again later.");
+      setAdvice("");
+    }
+  }
 
   return (
     <>
@@ -13,18 +32,16 @@ function App() {
         </a>
       </div>
       <h1>Deployment method</h1>
-      <p>Deployed with GitHub Pages</p>
-      <p>Deployed again</p>
-      <p>and again and again</p>
-      <p>and again and again and again</p>
-      <p>Lunch time!</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h3>Deployed with GitHub Pages</h3>
+      <p> V.1</p>
+      <h4>Let's deploy with some functions.</h4>
+
+      {/* Adding getAdvice */}
+      <button onClick={getAdvice}>Get advice</button> <br />
+      {error && <h4 style={{ color: "red" }}>{error}</h4>}
+      <h4>{advice}</h4>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
